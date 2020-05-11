e9dd9420a046944fc89d22ca159959718bde61cc
Amy Oulton <amyeoulton@gmail.com>
2020-05-11 09:39:45 -0700

    created all basic crud actions and views for ideas

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,2 +1,46 @@
 class IdeasController < ApplicationController
+
+    def new
+        @idea = Idea.new
+    end
+
+    def create
+        @idea = Idea.new(params.require(:idea).permit(:title, :description))
+        if @idea.save
+            redirect_to ideas_path
+          else
+            render :new
+          end
+    end
+
+    def index
+          @ideas = Idea.all.order('updated_at DESC')  
+    end
+
+    def show
+        id = params[:id]
+        @idea = Idea.find(id)
+    end
+
+    def edit
+        id = params[:id]
+        @idea = Idea.find(id)
+    end
+
+    def update
+        id = params[:id]
+        @idea = Idea.find(id)
+        if @idea.update(params.require(:idea).permit(:title, :description))
+          redirect_to idea_path(@idea)
+        else
+          render :edit
+        end
+    end
+
+    def destroy
+        id = params[:id]
+        @idea = Idea.find(id)
+        @idea.destroy
+        redirect_to ideas_path
+    end
 end
