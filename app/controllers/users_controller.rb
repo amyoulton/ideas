72c3d9ff4f1c56ebef21f2f51bf2a3fadf02636a
Amy Oulton <amyeoulton@gmail.com>
2020-05-11 09:59:30 -0700

    updated user controller methods, added shared navbar and ability to use flash messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,16 @@
 class UsersController < ApplicationController
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.new params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+        if @user.save
+          flash.delete(:warning)
+          redirect_to ideas_path
+        else
+          flash[:warning] = "unable to create user"
+          render :new
+        end
+      end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   
 resources :ideas
+resources :users, only: [:new, :create]
 
 end
