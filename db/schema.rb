9d9e4058874a99ca94661e6af9cdb6c4c29ca874
Amy Oulton <amyeoulton@gmail.com>
2020-05-11 10:39:29 -0700

    added review model and ran migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_11_165033) do
+ActiveRecord::Schema.define(version: 2020_05_11_173741) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,15 @@ ActiveRecord::Schema.define(version: 2020_05_11_165033) do
     t.index ["user_id"], name: "index_ideas_on_user_id"
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.text "body"
+    t.integer "rating"
+    t.bigint "idea_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["idea_id"], name: "index_reviews_on_idea_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
@@ -34,4 +43,5 @@ ActiveRecord::Schema.define(version: 2020_05_11_165033) do
   end
 
   add_foreign_key "ideas", "users"
+  add_foreign_key "reviews", "ideas"
 end
