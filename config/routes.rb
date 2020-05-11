f62ee3aa4ef38d4ec87a1b2ba07b488203b4d4c4
Amy Oulton <amyeoulton@gmail.com>
2020-05-11 10:42:13 -0700

    added associations between reviews and ideas in model and implemented routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   
 resources :ideas
 resources :users, only: [:new, :create]
-resource :session, only: [:new, :create, :destroy]
+resource :session, only: [:new, :create, :destroy] do
+  resources :reviews, only: [:create, :destroy]
+end
 
 end
